9d56b831809c45970dbb33e6e625cff0ed79d4a2
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2019-09-13 10:28:43 -0700

    let’s allow heroku to drain to the controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,11 @@
 class LogsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+  http_basic_authenticate_with name: ENV['WHOWASHERE_USER'],
+                               password: ENV['WHOWASHERE_PASSWORD'],
+                               only: :create
+
   def create
     Rails.logger.warn "log(#{request.body.read.inspect})"
+    head :ok
   end
 end
