7255c3d2e7b363bfb2d8d5fae63134868364c17a
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2019-09-17 09:41:28 -0700

    it should handle no user_ids

--- spec/controllers/api/sparklines_controller_spec.rb
+++ spec/controllers/api/sparklines_controller_spec.rb
@@ -21,14 +21,18 @@ RSpec.describe Api::SparklinesController, type: :controller do
 
       create_visit! user: user1, start_at: today - 1.day + 1.hour, seconds: 30.minutes
 
-      get :index, params: {school_id: school_id,
-                           ids: [user1.id, -1],}
+      get :index, params: { school_id: school_id,
+                            ids: [user1.id, -1], }
 
       expect(JSON.parse(response.body))
-        .to eq(
-          user1.id.to_s => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30.0, 0],
-          -1.to_s => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        )
+          .to eq(
+                  user1.id.to_s => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30.0, 0],
+                  -1.to_s => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+              )
+    end
+
+    it "should handle no user_ids" do
+      get :index, params: { school_id: school_id }
     end
   end
 end
